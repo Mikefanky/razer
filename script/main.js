554a40bed4009bb071f8e3d7ed6e19120192cbee
Dulce <npavlov@freshcode.org>
2018-05-17 14:10:13 +0300

    fix calculating price for hotel number

--- script/main.js
+++ script/main.js
@@ -1,14 +1,8 @@
 $( document ).ready(function() {
     $('.parallax-window').parallax();
-    $('.datepicker').datepicker({
-        format: 'mm/dd/yyyy',
-        startDate: '-3d'
-    });
-
-
-
+    $('#checkIn').change(render);
+    $('#checkOut').change(render);
     $('.brone').click(function(){
-        var d = $('div#someID').datepicker('getDate');
         var fio = $("input[name*='fio']").val();
         var email = $("input[name*='email']").val();
         var phone = $("input[name*='phone']").val();
@@ -18,7 +12,6 @@ $( document ).ready(function() {
         var checkIn = $("input[name*='check-in']").val();
         var checkOut= $("input[name*='check-out']").val();
         if(fio && email && phone && country && people && number){
-            // && checkIn && checkOut
             $.ajax({
                 url:'/brone',
                 type:'POST',
@@ -33,4 +26,20 @@ $( document ).ready(function() {
             });
         }
     });
+
+    function render(){
+        var checkIn = $('#checkIn').val();
+        var checkOut = $('#checkOut').val();
+        if(checkIn && checkOut){
+            checkIn = $('#checkIn').datepicker('getUTCDate');
+            checkOut = $('#checkOut').datepicker('getUTCDate');
+            var result =  checkOut - checkIn;
+            var result = Math.ceil(result / (1000 * 3600 * 24)*750);
+            $('#price').text(result+ "₴");
+        }
+        else{
+            $('#price').text("750 ₴");
+        }
+    }
+      
 });
